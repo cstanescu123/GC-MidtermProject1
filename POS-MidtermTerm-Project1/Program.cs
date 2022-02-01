07e2a5ece2cead50b5b423bec063ee53830a1419
Bob <bbice91@gmail.com>
2022-01-31 20:17:06 -0500

    Adding intial requirments and menu

--- POS-MidtermTerm-Project1/Program.cs
+++ POS-MidtermTerm-Project1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Corn, Kyle, and Bob!");
+﻿//REQUIREMENTS\\
+//
+//Product:
+//Class (name, category, description, price)
+//Point to 12 products stored in csv .txt
+//
+//Menu:
+///Allow choice of item(by int or char)
+///Allow user to choose quantity of item
+///Allow method to return to the menu
+//
+//Calculator:
+//Output to user a line total (item price * quantity)
+//Output the subtotal
+//Output the sales tax
+//Output the grand total
+
+
+
