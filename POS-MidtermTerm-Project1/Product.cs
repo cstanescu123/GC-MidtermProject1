6a75b21af3c9e0775be9a4fa11b5cd5f03f675e2
Kyle McIntosh <krmcintosh87@gmail.com>
2022-02-02 19:37:45 -0500

    updated product class to add constructor

--- POS-MidtermTerm-Project1/Product.cs
+++ POS-MidtermTerm-Project1/Product.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace POS_MidtermTerm_Project1
 {
-    abstract class Product
+    public class Product
     {
-      
-    
-    }
+        public string name { get; set; }
+        public string category { get; set; }
+        public string description { get; set; }
+        public double price { get; set; }
 
+        internal Product(string productName, string prodcutDescription, 
+                         string productCategory, double productPrice)
+        {
+            name = productName;
+            category = productCategory;
+            description = prodcutDescription;
+            price = productPrice;
+        }
+    }    
 }
