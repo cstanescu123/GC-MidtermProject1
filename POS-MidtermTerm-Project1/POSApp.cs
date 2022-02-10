c747039614992fef7e155340e3e1a74b4c9941b4
Kyle McIntosh <krmcintosh87@gmail.com>
2022-02-09 21:39:03 -0500

    cleaning up main class. Added validation, console fluff, and payment classes

--- POS-MidtermTerm-Project1/POSApp.cs
+++ POS-MidtermTerm-Project1/POSApp.cs
@@ -9,54 +9,32 @@ namespace POS_MidtermTerm_Project1
     internal class POSApp
     {
         public static void Run()
-        {
-
-            Console.SetWindowSize(82, 40);
-            Console.Title = "THE BURGER HUT II - Point of Sale Application";
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.BackgroundColor = ConsoleColor.Gray;
-            Console.Clear();
-
-            string userName = Menu.Welcome("Hello! Welcome to the Burger Hut II: Where our Menu starts at 10!\n", "Please enter the name for your Order: ");
-            bool anotherOrder = true;
-            while (anotherOrder)
-            {   
-                List<CartItem> cart = new List<CartItem>();
-                var userCart = Menu.SelectItemsForCart(Menu.ShowMenu("Here is our amazing list of delicious items!"), "Please enter the number corresponding to your food item!");
-                Console.WriteLine($"   Subtotal: ${Calc.GetSubTotal(userCart)}");
-                Console.WriteLine($"        Tax: ${Calc.GetTax(userCart)}");
-                Console.WriteLine($"Grand Total: ${Calc.GetGrandTotal(userCart)}");                
+        {            
+            ConsoleFluff.MakePrettyConsole();
+             try
+                {
+                    string userName = Menu.Welcome("Hello! Welcome to the Burger Hut II: Where our Menu starts at 10!\n", "Please enter the name for your Order: ");
+                    bool anotherOrder = true;
+                    while (anotherOrder)
+                    {   
+                        List<CartItem> cart = new List<CartItem>();
+                        var userCart = Menu.SelectItemsForCart(Menu.ShowMenu("Here is our amazing list of delicious items!"), "Please enter the number corresponding to your food item!");
+                        Console.WriteLine($"   Subtotal: ${Calc.GetSubTotal(userCart)}");
+                        Console.WriteLine($"        Tax: ${Calc.GetTax(userCart)}");
+                        Console.WriteLine($"Grand Total: ${Calc.GetGrandTotal(userCart)}");                
                  
-                string paymentType = Menu.AskForPayment("How would you like to pay for your awesome items?");
-                if (paymentType == "cash")
-                {                
-                    double cashOwed = CashRegister.GetPaidInCash(Calc.GetGrandTotal(userCart));                                
-                    if (cashOwed != 0) Console.WriteLine($"Change: ${cashOwed - 0}");
-                }
-                else if (paymentType == "check")
-                {
-                    CashRegister.GetPaidByCheck(Calc.GetGrandTotal(userCart));
-                }
-                else
-                {
-                    CashRegister.GetPaidWithCreditCard(Calc.GetGrandTotal(userCart));
-                }
+                        string paymentType = Menu.AskForPayment("How would you like to pay for your awesome items?");
+                        GetPaymentType.PaymentType(userCart, paymentType);
+                        Menu.ShowReceipt(userCart, paymentType);
 
-                Menu.ShowReceipt(userCart, paymentType);
-
-                Console.WriteLine("Would you like to place another order?");
-                string orderAgain = Console.ReadLine().Trim().ToLower();
-                if (orderAgain == "yes")
-                {
-                    anotherOrder = true;
+                        anotherOrder = TryValidate.GetValidation("Would you like to place another order?");
+                    }
+                    Console.WriteLine("Thank you for ordering with us, have a wonderful day!");
+                    } 
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("How did you break our amazing Burger Hut II Point of Sale System? Pleas contact tech support at our earliest convience.");
                 }
-                else
-                {
-                   anotherOrder = false;
-                }
-               
-            }
-            Console.WriteLine("Thank you for ordering with us, have a wonderful day!");
         }
     }
 }
